050484dfad17f0d7abbe7fd5a50414908356c617
cuberJ <2113159231@qq.com>
2021-01-10 21:24:51 +0800

    修订

--- git.sh
+++ git.sh
@@ -1,8 +1,8 @@
 git add *
 if $1 != ""
-    then git commit -m $1
+    then git commit -m $1;echo ${1}'成功加入仓库'
 else
-    git commit -m '修订'
+    git commit -m '修订';echo '未提供变量，但成功加入仓库'
 fi
 git push database master
-echo $1
+echo '成功上传'
