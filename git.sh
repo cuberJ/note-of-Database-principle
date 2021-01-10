5adabebe0deb7d4a63424f2208e50f97bebfeb9c
cuberJ <2113159231@qq.com>
2021-01-10 21:40:39 +0800

    修订

--- git.sh
+++ git.sh
@@ -6,11 +6,11 @@ else
 fi
 git push database master 2>/dev/null
 answer=$?
-echo $answer
+echo '上传结果的返回值为'$answer
 if [ "$answer"x = "0"x ]
     then echo '成功上传'
 elif [ "$answer"x = "128"x ]
     then echo '网络连接失败'
 else
-    echo '非网络因素的上传失败'
+    echo '非网络因素的上传失败';ping www.baidu.com;
 fi
