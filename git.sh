89f249ae9605197069fe066d5329dc8c134f309e
cuberJ <2113159231@qq.com>
2021-01-10 21:29:47 +0800

    修订

--- git.sh
+++ git.sh
@@ -5,10 +5,11 @@ else
     git commit -m '修订';echo '未提供变量，但成功加入仓库'
 fi
 git push database master
-echo $?
-if $? = '0'
+answer=$?
+echo answer
+if answer = '0'
     then echo '成功上传'
-elif $? = '128'
+elif answer = '128'
     then echo '网络连接失败'
 else
     echo '非网络因素的上传失败'
