53f58e063dce6046ee131968834a041728ad718f
cuberJ <2113159231@qq.com>
2021-01-11 12:51:18 +0800

    修订

--- git.sh
+++ git.sh
@@ -6,11 +6,23 @@ else
 fi
 git push database master 2>/dev/null
 answer=$?
-echo '上传结果的返回值为'$answer
+echo 'gitee的上传结果的返回值为'$answer
 if [ "$answer"x = "0"x ]
     then echo '成功上传'
 elif [ "$answer"x = "128"x ]
     then echo '网络连接失败'
 else
-    echo '非网络因素的上传失败';ping www.baidu.com;
+    echo '非网络因素的上传失败';
 fi
+git push gdata master 2>/dev/null
+answer=$?
+echo 'github的上传结果返回值为'$answer
+if [ "$answer"x = "0"x ]
+    then echo github上传成功
+elif [ "$answer"x = "128"x ]
+    then echo github网络连接失败
+else
+    echo 非网络连接问题导致的上传失败
+fi
+echo 测试百度ping结果...
+ping www.baidu.com
